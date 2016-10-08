ec27b79fb88f6554ddedfb8e73ba8c6f24bcf9bc
Boris Reinos <boreinos@gmail.com>
2016-10-07 23:34:33 -0400

    dummy astar_listener removed

--- astar_listener.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from geometry_msgs.msg import Pose
-#from geometry_msgs.msg import Goal
-
-# listener function:
-def listener():
-    rospy.init_node('goal_listener', anonymous=True)
-    rospy.Subscriber("/move_base_simple/goal", PoseStamped, callback)
-    rospy.spin()
