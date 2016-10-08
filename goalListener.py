8ed947e64f619b878bb7a86accc0cd13df6aee86
Boris Reinos <boreinos@gmail.com>
2016-10-08 00:24:28 -0400

    skeleton listener structure

--- goalListener.py
+++ goalListener.py
@@ -4,17 +4,36 @@ import rospy
 from matplotlib import pyplot
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import MapMetaData
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
 import numpy as np
 
+map_array= np.zeros((1,1))
+goal = None
+ready = False
+
+def callback3(data):
+    #get goal, raise flag and allow current pose to handle
+    rospy.loginfo(rospy.get_caller_id()+" goal recieved" )
+    goal = data.data
+    ready = True
+
+def callback2(data):
+    if ready:
+        #acknowledge pose was recieved:
+        rospy.loginfo(rospy.get_caller_id()+" pose map received")
+        #do something ...
+        ready = False
+    return 
+
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id()+"message received")
+    rospy.loginfo(rospy.get_caller_id()+" map data received") #acknowledge for degub purposes
     width=data.info.width
     height=data.info.height
     image=np.zeros((height,width))
     rawArray = data.data
     length = len(rawArray)
     start = 0
-
     for i in range(height):
         n=(i+1)*width
         row = list(rawArray[start:n])
@@ -27,12 +46,15 @@ def callback(data):
                 image[i,k]=256
         start = n+1
     reversed_image = image[::-1]
-    pyplot.imshow(reversed_image, pyplot.cm.gray)
-    pyplot.show()  
+    map_array = reveresed_image #save map
+    #pyplot.imshow(reversed_image, pyplot.cm.gray)
+    #pyplot.show()  
 
 def listener():
     rospy.init_node('listener',anonymous=True)
     rospy.Subscriber("map", OccupancyGrid, callback)
+    rospy.Subscriber("RosAria/pose_map",Odometry, callback2)
+    rospy.Subscriber("move_base_simple/goal",PoseStamped,callback3)
     rospy.spin()
 #--------------------------------------------------------------
 if __name__ == '__main__':
